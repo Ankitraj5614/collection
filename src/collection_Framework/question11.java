133ab12a40dc713d4f626979f559d95057f48708
Ankit Raj <ankit91ece>
2025-07-16 22:23:26 +0530

    add prime number

--- src/collection_Framework/question11.java
+++ src/collection_Framework/question11.java
@@ -1,6 +1,7 @@
 package collection_Framework;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /*WAJP to create a collection of integers and count
  *  and print all the prime numbers available in collection.
@@ -11,11 +12,41 @@ public class question11 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ArrayList<Integer> a= new ArrayList<>();
-		a.add(421);
-		a.add(4543);
-		a.add(4543);
-		a.add(42);
+		a.add(17);
 		a.add(43);
+		a.add(13);
+		a.add(42);
+		a.add(7);
+		int count =0;
+		Iterator<Integer> b=a.iterator();
+		while(b.hasNext())
+		{
+			int n=b.next();
+			if(getPrime(n))
+				count++;
+			else
+				b.remove();
+		}
+		
+		System.out.println("count the prime number "+count);
+		System.out.println("count the prime number"+a);
+		
+	}
+
+	public static boolean getPrime(int n) {
+		if(n<=2)
+			return false;
+		else if(n==2||n==3)
+			return true;
+		else if(n%2==0)
+			return false;
+		int i=3;
+		while(i*i<=n) {
+			if(n%i==0)
+				return false;
+			i+=2;
+		}
+		return true;
 	}
 
 }
